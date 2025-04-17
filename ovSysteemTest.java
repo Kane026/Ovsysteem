e1102cec0a0e1b47f7e438fc3c8784c9f2d62ebe
Kane026 <frentzkane@gmail.com>
2025-04-17 23:20:41 +0200

    Update ovSysteemTest.java

--- ovSysteemTest.java
+++ ovSysteemTest.java
@@ -1,15 +1,12 @@
 package ovSysteem;
 
-public class ovSysteemTest
-{
-
-    public static void main(String[] args)
-    {
-	ovChipkaart chipkaart = new ovChipkaart("1234", 10, false);
-
-	inchecken incheckSysteem = new inchecken();
-	
-	incheckSysteem.inchecken(chipkaart);
+public class ovSysteemTest {
+    public static void main(String[] args) {
+	// maakt nieuwe chipkaart aan door de methode ovKaart() aan te roepen
+        ovChipkaart chipkaart = ovChipkaart.ovKaart();
+        //maakt een nieuwe ov paal voor station Nijmegen
+        ovPaal nijmegen = new ovPaal("Nijmegen");
+        //roept de functie op van de ovpaal nijmegen en pakt hierbij de eerder gemaakte chipkaart zodat deze hierop kan worden uitgevoerd
+        nijmegen.ovPaalFunctie(chipkaart);
     }
-
 }
