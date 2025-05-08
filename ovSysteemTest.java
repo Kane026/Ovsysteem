c3fdc17d70a73d6656042803e2303fba68202075
Kane026 <frentzkane@gmail.com>
2025-05-08 14:25:28 +0200

    Update ovSysteemTest.java

--- ovSysteemTest.java
+++ ovSysteemTest.java
@@ -1,12 +1,37 @@
 package ovSysteem;
 
-public class ovSysteemTest {
-    public static void main(String[] args) {
+import java.util.Scanner;
+
+public class ovSysteemTest
+{
+    public static void main(String[] args)
+    {
 	// maakt nieuwe chipkaart aan door de methode ovKaart() aan te roepen
-        ovChipkaart chipkaart = ovChipkaart.ovKaart();
-        //maakt een nieuwe ov paal voor station Nijmegen
-        ovPaal nijmegen = new ovPaal("Nijmegen");
-        //roept de functie op van de ovpaal nijmegen en pakt hierbij de eerder gemaakte chipkaart zodat deze hierop kan worden uitgevoerd
-        nijmegen.ovPaalFunctie(chipkaart);
+	ovChipkaart chipkaart = ovChipkaart.ovKaart();
+	// maakt een nieuwe ov paal voor beginhalte
+	ovPaal beginHalte = new ovPaal("Elst");
+	// maakt ovpaal aan voor de eindhalte
+	ovPaal eindHalte = new ovPaal("Nijmegen");
+	Scanner scanner = new Scanner(System.in);
+//zorgt ervoor dat programma blijft draaien tot het gesloten wordt
+	while (true)
+	{
+	    System.out.println("Wat wilt uw doen? Inchecken/Uitchecken/Opwaarderen");
+	    String input = scanner.nextLine().toLowerCase();
+
+	    if (input.contains("inchecken"))
+	    {
+		beginHalte.inchecken(chipkaart);
+	    } else if (input.contains("uitchecken"))
+	    {
+		eindHalte.uitchecken(chipkaart);
+	    } else if (input.contains("opwaarderen"))
+	    {
+		eindHalte.opwaarderen(chipkaart, scanner);
+	    }
+
+	}
     }
 }
+
+
